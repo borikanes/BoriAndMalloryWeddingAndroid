20f9be4588423fb3ea39dfd2c0a992ff062eeac9
borikanes <ooludemi@terpmail.umd.edu>
2018-08-10 04:30:04 -0400

    removed unused imports

--- app/src/main/java/com/mobile/boriandmallory/boriandmalloryweddingandroid/tabfragments/HomeFragment.kt
+++ app/src/main/java/com/mobile/boriandmallory/boriandmalloryweddingandroid/tabfragments/HomeFragment.kt
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
 import com.mobile.boriandmallory.boriandmalloryweddingandroid.R
 import kotlinx.android.synthetic.main.fragment_home.*
 import java.text.SimpleDateFormat
