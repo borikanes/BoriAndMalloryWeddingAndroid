e82c3f856b909b5f30749bf63b83fbaee4174b4f
Mike Ferraco <mike@upside.com>
2018-08-21 08:22:37 -0400

    searching in seating chart

--- app/src/main/java/com/mobile/boriandmallory/boriandmalloryweddingandroid/tabfragments/seating/SeatingAdapter.kt
+++ app/src/main/java/com/mobile/boriandmallory/boriandmalloryweddingandroid/tabfragments/seating/SeatingAdapter.kt
@@ -4,11 +4,21 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import com.mobile.boriandmallory.boriandmalloryweddingandroid.R
 import com.mobile.boriandmallory.boriandmalloryweddingandroid.models.Seating
 import kotlinx.android.synthetic.main.item_seating.view.*
 
-class SeatingAdapter(private val seating: List<Seating>) : RecyclerView.Adapter<SeatingAdapter.SeatingViewHolder>() {
+class SeatingAdapter(private val seating: List<Seating>) :
+        RecyclerView.Adapter<SeatingAdapter.SeatingViewHolder>(),
+        Filterable {
+
+    companion object {
+        const val NULL = "null"
+    }
+
+    private var seatingFiltered = seating
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeatingViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_seating, parent, false)
@@ -16,10 +26,31 @@ class SeatingAdapter(private val seating: List<Seating>) : RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: SeatingViewHolder, position: Int) {
-        holder.bind(seating[position])
+        holder.bind(seatingFiltered[position])
     }
 
-    override fun getItemCount() = seating.size
+    override fun getItemCount() = seatingFiltered.size
+
+    override fun getFilter(): Filter = object : Filter() {
+        override fun performFiltering(query: CharSequence?): FilterResults {
+            val queryString: String = query.toString()
+            seatingFiltered = if (queryString.isEmpty() || queryString == NULL) {
+                seating
+            } else {
+                seating.filter {
+                    it.name.toLowerCase().contains(queryString.toLowerCase()) || it.table.toLowerCase() == queryString.toLowerCase()
+                }
+            }
+            val filterResults = FilterResults()
+            filterResults.values = seatingFiltered
+            return filterResults
+        }
+
+        override fun publishResults(query: CharSequence?, result: FilterResults?) {
+            seatingFiltered = (result?.values as? List<*>)?.filterIsInstance<Seating>() ?: listOf()
+            notifyDataSetChanged()
+        }
+    }
 
     inner class SeatingViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
--- app/src/main/java/com/mobile/boriandmallory/boriandmalloryweddingandroid/tabfragments/seating/SeatingFragment.kt
+++ app/src/main/java/com/mobile/boriandmallory/boriandmalloryweddingandroid/tabfragments/seating/SeatingFragment.kt
@@ -1,14 +1,15 @@
 package com.mobile.boriandmallory.boriandmalloryweddingandroid.tabfragments.seating
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.SearchView
+import android.view.*
 import com.mobile.boriandmallory.boriandmalloryweddingandroid.R
 import com.mobile.boriandmallory.boriandmalloryweddingandroid.models.Seating
-import com.mobile.boriandmallory.boriandmalloryweddingandroid.networking.RestClient
 import kotlinx.android.synthetic.main.fragment_seating.*
 
 /**
@@ -18,6 +19,9 @@ import kotlinx.android.synthetic.main.fragment_seating.*
 class SeatingFragment : Fragment(), SeatingContract.View {
 
     private val presenter = SeatingPresenter()
+    private var adapter: SeatingAdapter? = null
+    private var searchView: SearchView? = null
+    private var currentQuery: String? = null
 
     companion object {
 
@@ -35,7 +39,47 @@ class SeatingFragment : Fragment(), SeatingContract.View {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_seating, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.search_menu, menu)
+
+        // set up search view
+        val searchManager = context?.getSystemService(Context.SEARCH_SERVICE) as? SearchManager
+        searchView = menu?.findItem(R.id.action_search)?.actionView as? SearchView
+        searchView?.setSearchableInfo(searchManager?.getSearchableInfo(activity?.componentName))
+        searchView?.maxWidth = Integer.MAX_VALUE
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(query: String?): Boolean {
+                currentQuery = query
+                adapter?.filter?.filter(query)
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                currentQuery = query
+                adapter?.filter?.filter(query)
+                return false
+            }
+        })
+
+        searchView?.setQuery(currentQuery, true)
+
+        if (currentQuery.isNullOrEmpty()) {
+            searchView?.isIconified = true
+            searchView?.clearFocus()
+        } else {
+            searchView?.isIconified = false
+        }
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+        R.id.action_search -> true
+        else -> super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,6 +87,8 @@ class SeatingFragment : Fragment(), SeatingContract.View {
         presenter.getSeating()
 
         seating_recycler_view.layoutManager = LinearLayoutManager(context)
+        seating_recycler_view.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        searchView?.isIconified = true
     }
 
     override fun onStop() {
@@ -51,7 +97,8 @@ class SeatingFragment : Fragment(), SeatingContract.View {
     }
 
     override fun setupSeating(seating: List<Seating>) {
-        seating_recycler_view.adapter = SeatingAdapter(seating)
+        adapter = SeatingAdapter(seating)
+        seating_recycler_view.adapter = adapter
     }
 
     override fun showProgress(show: Boolean) {
